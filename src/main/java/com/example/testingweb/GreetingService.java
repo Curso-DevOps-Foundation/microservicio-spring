597592acb421eebddf751ebccc7c30790f0bbe11
LourdesTorres <110629424+LourdesTorres@users.noreply.github.com>
2022-09-10 00:09:56 -0300

    Update GreetingService.java

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -2,10 +2,9 @@ package com.example.testingweb;
 
 import org.springframework.stereotype.Service;
 
-public static final String GREETINGS = "Finaliza el curso DevOps Foundation v2 - Lourdes Torres";
-
 @Service
 public class GreetingService {
+	public static final String GREETINGS = "Finaliza el curso DevOps Foundation v2 - Lourdes Torres";
 	public String greet() {
 		return GREETINGS;
 	}
